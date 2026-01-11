bcfa222a38ed73646064300eeaa2a4126fa99430
Bindushree B <bindushree1978@gmail.com>
2026-01-11 18:59:16 +0530

    re-updated vite.config.ts for ES modules compatibility

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,30 @@
+// import react from '@vitejs/plugin-react';
+// import { defineConfig } from 'vite';
+// import { fileURLToPath } from 'url';
+// import path from 'path';
+
+// const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+// export default defineConfig({
+//   plugins: [react()],
+//   resolve: {
+//     alias: {
+//       '@': path.resolve(__dirname, 'src'),
+//     },
+//   },
+// });
+
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
-import { fileURLToPath } from 'url';
-import path from 'path';
-
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
+import { fileURLToPath, URL } from 'node:url';
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src'),
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
 });
+
 
